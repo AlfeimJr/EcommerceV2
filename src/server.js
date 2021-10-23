ed878111405450bfb411a908fc2fdcf27ca9ddc5
LeoScripts <leandroguitarjesus89@gmail.com>
2021-10-23 17:02:40 -0300

    correçoes

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
-const rotas = require('./routes');
+const rotas = require('./routes/index');
 
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.resolve("views"));
+app.set('views', path.resolve("src","views"));
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve("src",'public')));
 app.use(rotas);
 
 
-app.listen(3000);
+app.listen(3000, ()=>{
+    console.log('server in:', 'http://localhost:3000')
+});
