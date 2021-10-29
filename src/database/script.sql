133eb77689d5fdbdd0f6d2f8473bc34d3761425f
LeoScripts <leandroguitarjesus89@gmail.com>
2021-10-29 16:39:24 -0300

    correção de bugs no script do banco de dados

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,34 +1,13 @@
+
+-- apos testes e correção
+-- o codigo nao esta bem organizado para que todos 
+-- venhao a emtender a logica falo em questao das foreign key
 CREATE DATABASE devgames;
 
-CREATE TABLE devgames.produto (
-  id INT(10) PRIMARY KEY auto_increment,
-  nome VARCHAR(200),
-  fabricante_id INT(10)FOREIGN KEY,
-  preco VARCHAR(200),
-  imagem VARCHAR(200),
-  quantidade INT(10),
-  ativo tinyINT(1),
-  created_at TIMESTAMP,
-  content VARCHAR(200)  
-);
+use devgames;
 
-CREATE TABLE devgames.fabricante (
-  id INT(10) PRIMARY KEY auto_increment,
-  nome VARCHAR(200),
-  content VARCHAR(200),
-  produto_id INT(10)FOREIGN KEY,
-  created_at TIMESTAMP
-);
-
-CREATE TABLE devgames.produto_oferta (
-  id INT(10) PRIMARY KEY auto_increment,
-  produto_id INT(10)FOREIGN KEY,
-  content VARCHAR(200),
-  created_at TIMESTAMP
-);
-
-CREATE TABLE devgames.usuario(
-  id INT(10) PRIMARY KEY auto_increment,
+CREATE TABLE usuario(
+  id INTEGER PRIMARY KEY auto_increment,
   nome VARCHAR(200),
   email VARCHAR(200),
   senha VARCHAR(200),
@@ -38,35 +17,68 @@ CREATE TABLE devgames.usuario(
   content VARCHAR(200)  
 );
 
-CREATE TABLE devgames.carrinho(
-  id INT(10) PRIMARY KEY auto_increment,
-  produto_id INT(10)FOREIGN KEY,
+CREATE TABLE fabricante (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(200),
+  content VARCHAR(200),
+  created_at TIMESTAMP
+);
+
+
+CREATE TABLE devgames.produto (
+  id INT(10) PRIMARY KEY AUTO_INCREMENT,
+  id_fabricante INTEGER,
+  FOREIGN KEY (id_fabricante) REFERENCES fabricante(id),
+  nome VARCHAR(200),
+  preco VARCHAR(200),
+  imagem VARCHAR(200),
+  quantidade INT(10),
+  ativo tinyINT(1),
+  created_at TIMESTAMP,
+  content VARCHAR(200) 
+);
+
+CREATE TABLE promocao (
+  id INT(10) PRIMARY KEY AUTO_INCREMENT,
+  id_produto INTEGER,
+  FOREIGN KEY (id_produto)REFERENCES produto(id),
+  content VARCHAR(200),
+  created_at TIMESTAMP
+);
+
+CREATE TABLE carrinho(
+  id INT(10) PRIMARY KEY AUTO_INCREMENT,
+  id_produto INTEGER,
+  FOREIGN KEY(id_produto)REFERENCES produto(id),
   quantidade VARCHAR(200),
   preco_total float(2),
   content VARCHAR(200),
   created_at TIMESTAMP
 );
 
-CREATE TABLE devgames.compra(
-  id INT(10) PRIMARY KEY auto_increment,
-  usuario_id INT(10)FOREIGN KEY,
-  produto_id INT(10)FOREIGN KEY,
+CREATE TABLE compra(
+  id INT(10) PRIMARY KEY AUTO_INCREMENT,
+  id_carrinho INTEGER,
+  FOREIGN KEY(id_carrinho)REFERENCES carrinho(id),
+  id_usuario INTEGER,
+  FOREIGN KEY (id_usuario)REFERENCES usuario(id),
+  id_produto INTEGER,
+  FOREIGN KEY(id_produto)REFERENCES produto(id),
   rua VARCHAR(200),
   numero INT(10),
   bairro VARCHAR(200),
   cidade VARCHAR(200),
   estado VARCHAR(200),  
   cep INT(10),
-  carrinho_id INT(10)FOREIGN KEY,
   pais VARCHAR(200),
   content VARCHAR(200),
   created_at TIMESTAMP
 );
 
-CREATE TABLE devgames.categoria(
-  id INT(10) PRIMARY KEY auto_increment,
-  produto_id INT(10)FOREIGN KEY,
+CREATE TABLE categoria(
+  id INT(10) PRIMARY KEY AUTO_INCREMENT,
+  id_produto INTEGER,
+  FOREIGN KEY(id_produto)REFERENCES produto(id),
   content VARCHAR(200),
   created_at TIMESTAMP 
 );
-
