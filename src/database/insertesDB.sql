d4cc0d12d8ae5a1738d3b73a55391dcc3d12257d
LeoScripts <leandroguitarjesus89@gmail.com>
2021-11-08 17:50:25 -0300

    insert fabricante

--- src/database/insertesDB.sql
+++ src/database/insertesDB.sql
@@ -1,14 +1,12 @@
-INSERT INTO devgames.produto(nome,preco,imagem,quantidade,ativo)
-VALUES ('teclado','30.00','adfs','2',0);
+INSERT INTO devgames.produto(nome,preco,imagem,quantidade,ativo)VALUES 
+('teclado','30.00','adfs','2',0),
+('mouse','15.00','adfs','2',0),
+('monitor','400.00','adfs','2',1),
+('cpu','998.00','adfs','2',0),
+('fone de ouvido','90.00','adfs','2',1);
 
-INSERT INTO devgames.produto(nome,preco,imagem,quantidade,ativo)
-VALUES ('mouse','15.00','adfs','2',0);
 
-INSERT INTO devgames.produto(nome,preco,imagem,quantidade,ativo)
-VALUES ('monitor','400.00','adfs','2',1);
-
-INSERT INTO devgames.produto(nome,preco,imagem,quantidade,ativo)
-VALUES ('cpu','998.00','adfs','2',0);
-
-INSERT INTO devgames.produto(nome,preco,imagem,quantidade,ativo)
-VALUES ('fone de ouvido','90.00','adfs','2',1);
+INSERT INTO fabricante(nome) VALUES
+('fabricante um' ),
+('fabricante dois' ),
+('fabricante  tres' );
